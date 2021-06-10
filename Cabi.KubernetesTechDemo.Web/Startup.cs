42454cd25877e5f50d9d5335ee7de1ffd166f8b6
Neil Docherty <N.Docherty@cabi.org>
2021-06-10 18:56:21 +0100

    Initial health check added

--- Cabi.KubernetesTechDemo.Web/Startup.cs
+++ Cabi.KubernetesTechDemo.Web/Startup.cs
@@ -10,7 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
 using Serilog.Events;
 
@@ -29,6 +31,9 @@ namespace Cabi.KubernetesTechDemo.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks()
+                .AddCheck("Test", () => HealthCheckResult.Healthy("All is OK"), new[] { "kubernetes" });
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
@@ -59,6 +64,27 @@ namespace Cabi.KubernetesTechDemo.Web
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    ResultStatusCodes =
+                    {
+                        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                    }
+                });
+
+                endpoints.MapHealthChecks("/health/kubernetes", new HealthCheckOptions()
+                {
+                    ResultStatusCodes =
+                    {
+                        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                    },
+                    Predicate = (check) => check.Tags.Contains("kubernetes")
+                });
             });
         }
 
